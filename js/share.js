13b93db830c38f309bee42c4d798e37af50f2d2c
TheHDLabs <thehdlabs@gmail.com>
2025-04-11 12:35:56 +0530

    insta share button

--- js/share.js
+++ js/share.js
@@ -14,3 +14,19 @@ document.getElementById("shareBtn").addEventListener("click", async () => {
     alert("Web Share API is not supported in your browser");
   }
 });
+
+document.getElementById("instaShareBtn").addEventListener("click", () => {
+  const imageUrl = document.querySelector(".portrait-image").src;
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+  if (isMobile) {
+    window.location.href = `instagram://library?AssetPath=${encodeURIComponent(
+      imageUrl
+    )}`;
+    setTimeout(() => {
+      window.location.href = "https://www.instagram.com";
+    }, 2000);
+  } else {
+    alert("Instagram sharing is only available on mobile devices");
+  }
+});
